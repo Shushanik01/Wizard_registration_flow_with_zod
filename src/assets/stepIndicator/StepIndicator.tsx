45f9156ebeddfe07b533908dd1de26695b500d2b
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-04 01:18:32 +0400

    feat: create step indicator

---
+++ src/assets/stepIndicator/StepIndicator.tsx
@@ -0,0 +1,25 @@
+import styles from './stepIndicatoe.module.css'
+
+interface StepIndicatorProps {
+    totalSteps: number,
+    currentStep: number
+};
+
+const StepIndicator = ({ totalSteps, currentStep }: StepIndicatorProps) => {
+
+    return (
+        <div className={styles.wrapper}>
+            {Array.from({ length: totalSteps }, (_, i) => (
+                <>
+                    <div
+                        key={i}
+                        className={`${styles.circle} ${i + 1 <= currentStep ? styles.active : ''}`}
+                    >
+                        {i + 1}
+                    </div>
+                    {i + 1 < totalSteps && <div className={styles.line} />}
+                </>
+            ))}
+        </div>
+    )
+}
