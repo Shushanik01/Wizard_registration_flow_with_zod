24c703949e43f8ebec51767ca095ab025c6fc48d
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-04 00:35:11 +0400

    fix: fix calendar position and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import FormInput from "./assets/formField/formInput";
-import SelectForm from './assets/formSelect/SelectForm'
+import SelectForm from './assets/formSelect/SelectForm';
+import Calendar from "./assets/calendar/Calendar";
+
 function App() {
 
   return (
     <>
       <FormInput label="this is label" type="text" placeholder="this is placeholder" />
       <SelectForm placeholder="the plaveholder" options={[1,2,3,4,5]}/>
+      <Calendar/>
     </>
   )
 }
--- src/assets/calendar/Calendar.tsx
+++ src/assets/calendar/Calendar.tsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import calendarIcon from '../../svg/calendar-symbol.svg';
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-
+import styles from './calendar.module.css'
 
 const Calendar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
     const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
-    const handleCalendarBehaviour = ()=>{
+    const handleCalendarBehaviour = () => {
         setIsOpen(prevState => !prevState)
     }
 
     return (
-        <>
+        <div className={styles.wrapper}>
             <button type='button' onClick={handleCalendarBehaviour}>
                 <img src={calendarIcon} />
             </button>
             {isOpen && (
-                <DatePicker
-                selected={selectedDate}
-                onChange={(date)=>{
-                    setSelectedDate(date)
-                    setIsOpen(false)
-                }}
-                inline
-                />
+                <div className={styles.datepickerWrapper}>
+                    <DatePicker
+                        selected={selectedDate}
+                        onChange={(date) => {
+                            setSelectedDate(date)
+                            setIsOpen(false)
+                        }}
+                        inline
+                    />
+                </div>
             )}
-        </>
+        </div>
     )
 }
 export default Calendar
